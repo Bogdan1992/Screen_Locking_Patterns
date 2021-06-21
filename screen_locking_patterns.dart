be81a8111cd6697b36cfdee92b8c56f1463d5881
bogdan.trotsko <trotskobogdan@gmail.com>
2021-06-21 19:05:56 +0300

    1.0

---
+++ screen_locking_patterns.dart
@@ -0,0 +1,33 @@
+void main(List<String> arguments) {
+  print(countPatternsFrom('A', 3));
+}
+
+Object countPatternsFrom(String f, int l) {
+  var used_point = [];
+  var current_point = [];
+  var point_possible = {
+    'A': ['B', 'E', 'F', 'D', 'H'],
+    'B': ['A', 'C', 'E', 'F', 'D', 'G', 'I'],
+    'C': ['B', 'D', 'E', 'F', 'H'],
+    'D': ['A', 'B', 'C', 'E', 'G', 'H', 'I'],
+    'E': ['A', 'B', 'C', 'D', 'F', 'G', 'H', 'I'],
+    'F': ['A', 'B', 'C', 'E', 'G', 'H', 'I'],
+    'G': ['D', 'B', 'E', 'F', 'H'],
+    'H': ['G', 'D', 'A', 'E', 'C', 'F', 'I'],
+    'I': ['H', 'D', 'E', 'B', 'F']
+  };
+  var num_combinations = 0;
+  for (var i = 0; i < l - 1; i++) {
+    if (i == 0) {
+      used_point.add(f);
+      current_point.add(point_possible[f]);
+      num_combinations += point_possible[f]!.length;
+    } else {
+      for (var a = 0; a < current_point[i - 1].length; a++) {
+        current_point[i - 1][a];
+      }
+      used_point.addAll(current_point[i - 1]);
+    }
+  }
+  return current_point;
+}
